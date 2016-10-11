6af5cc9de812aeaecc4b37de0d0c7e90d8fd5f1e
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-10-11 08:08:06 -0500

    side menu appears

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic']).run(function ($ionicPlatform) {
+angular.module('ionic-conduit', ['ionic']).run(function ($ionicPlatform) {
   $ionicPlatform.ready(function () {
     if (window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,4 +21,28 @@ angular.module('starter', ['ionic']).run(function ($ionicPlatform) {
       StatusBar.styleDefault();
     }
   });
-});
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    // controller: 'AppCtrl'
+  })
+
+  .state('app.home', {
+    url: '/home',
+    views: {
+      templateUrl: 'home.html',
+      // controller: 'HomeCtrl'
+    }
+  
+  })
+
+  
+
+  $urlRouterProvider.otherwise('app/home');
+})
